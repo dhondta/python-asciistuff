664bf29a63083e572a8e1858a03de6784d4fac24
dhondta <alexandre.dhondt@gmail.com>
2019-10-14 21:11:50 +0200

    Updated version in __info__

--- asciistuff/__info__.py
+++ asciistuff/__info__.py
@@ -4,6 +4,6 @@
 """
 
 __author__    = "Alexandre D'Hondt"
-__version__   = "1.0.0"
+__version__   = "1.0.1"
 __copyright__ = "© 2019 A. D'Hondt"
 __license__   = "AGPLv3 (http://www.gnu.org/licenses/agpl.html)"
